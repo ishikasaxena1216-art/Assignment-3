42e00a7d2dd477717f7d4a7aaa9346db7bbcbd6c
CodeVibesOnly <ishikasaxena@ISHIKAs-MacBook-Air.local>
2025-12-03 16:28:56 +0530

    Updated code

--- tic_tac_toe.c
+++ tic_tac_toe.c
@@ -1,89 +1,114 @@
 #include <stdio.h>
-#include <conio.h>
-#include <windows.h>
-#include <stdlib.h>
-#include <time.h>
-/*for bg sound*/
-#include <mmsystem.h>
 
-/*to remove flicker*/
-void clear_screen_fast() {
-    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
-    COORD pos = {0, 0};
-    SetConsoleCursorPosition(h, pos);
+char board[3][3];
+
+void initBoard() {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            board[i][j] = ' ';
+        }
+    }
+}
+
+void printBoard() {
+    printf("\n    1   2   3  (column)\n");
+    for (int i = 0; i < 3; i++) {
+        printf("  +---+---+---+\n");
+        printf("%d | %c | %c | %c |\n", i + 1, board[i][0], board[i][1], board[i][2]);
+    }
+    printf("  +---+---+---+\n");
+    printf(" (row)\n\n");
+}
+
+int checkWin() {
+    // rows
+    for (int i = 0; i < 3; i++) {
+        if (board[i][0] != ' ' &&
+            board[i][0] == board[i][1] &&
+            board[i][1] == board[i][2])
+            return 1;
+    }
+    // columns
+    for (int j = 0; j < 3; j++) {
+        if (board[0][j] != ' ' &&
+            board[0][j] == board[1][j] &&
+            board[1][j] == board[2][j])
+            return 1;
+    }
+    // diagonals
+    if (board[0][0] != ' ' &&
+        board[0][0] == board[1][1] &&
+        board[1][1] == board[2][2])
+        return 1;
+
+    if (board[0][2] != ' ' &&
+        board[0][2] == board[1][1] &&
+        board[1][1] == board[2][0])
+        return 1;
+
+    return 0;
+}
+
+int isBoardFull() {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (board[i][j] == ' ')
+                return 0;
+        }
+    }
+    return 1;
 }
 
 int main() {
-	system("color 4F");
-	PlaySound(TEXT("bg.wav"), NULL, SND_ASYNC | SND_LOOP);
-    srand(time(0));
+    int row, col;
+    char currentPlayer = 'X';
 
-    int x = 1;              // player position (0 to 2)
-    int step = 0;           // obstacle vertical movement
-    int obstaclePos = rand() % 3;   // 0,1,2 lane
+    initBoard();
+
+    printf("Tic-Tac-Toe Game in C\n");
+    printf("Player 1: X\nPlayer 2: O\n");
+    printf("Enter moves as: row column (e.g. 1 1 for top-left)\n");
 
     while (1) {
+        printBoard();
+        printf("Player %c, enter row and column (1-3 1-3): ", currentPlayer);
 
-        // ---- INPUT ----
-        if (_kbhit()) {
-            char ch = getch();
-
-            if (ch == 75 && x > 0)        // LEFT arrow
-                x--;
-
-            if (ch == 77 && x < 2)        // RIGHT arrow
-                x++;
+        if (scanf("%d %d", &row, &col) != 2) {
+            printf("\nInvalid input, exiting.\n");
+            return 0;
         }
 
-        // ---- DRAW ----
-		// system("cls");
-		clear_screen_fast();
-        printf("|--- --- ---|\n");
-
-        for (int i = 0; i < 10; i++) {
-            if (i == step) {
-
-                if (obstaclePos == 0)
-                    printf("| %c        |\n", 1);
-
-                else if (obstaclePos == 1)
-                    printf("|     %c    |\n", 1);
-
-                else if (obstaclePos == 2)
-                    printf("|        %c |\n", 1);
-
-            } else {
-                printf("|           |\n");
-            }
+        // range check
+        if (row < 1 || row > 3 || col < 1 || col > 3) {
+            printf("Invalid position! Please enter values between 1 and 3.\n");
+            continue;
         }
 
-        // ---- PLAYER ----
-        if (x == 0)
-            printf("| %c         |\n", 6);
-        else if (x == 1)
-            printf("|     %c     |\n", 6);
-        else if (x == 2)
-            printf("|        %c  |\n", 6);
+        // convert to 0-based index
+        row--;
+        col--;
 
-        // ---- COLLISION ----
-        if (step == 10 && x == obstaclePos) {
-        	PlaySound(NULL, NULL, 0);  // stop background
-			PlaySound(TEXT("impact.wav"), NULL, SND_ASYNC);
-        	Sleep(2500);
-            printf("\nGAME OVER!\n");
+        if (board[row][col] != ' ') {
+            printf("Cell already taken! Choose another.\n");
+            continue;
+        }
+
+        board[row][col] = currentPlayer;
+
+        if (checkWin()) {
+            printBoard();
+            printf("🎉 Player %c wins!\n", currentPlayer);
             break;
         }
 
-        Sleep(120);
-
-        // Move obstacle down
-        step++;
-
-        // Reset when reaches bottom
-        if (step > 10) {
-            step = 0;
-            obstaclePos = rand() % 3; // new lane
+        if (isBoardFull()) {
+            printBoard();
+            printf("It's a draw!\n");
+            break;
         }
+
+        // switch player
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
     }
 
     return 0;
